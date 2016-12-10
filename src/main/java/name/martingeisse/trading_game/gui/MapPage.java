b9b3786132b765de1f7e66e393a154ca29d66c09
Martin Geisse <martingeisse@googlemail.com>
2016-12-10 18:47:11 +0100

    map page: sidebar; mouse dragging movement

--- src/main/java/name/martingeisse/trading_game/gui/MapPage.java
+++ src/main/java/name/martingeisse/trading_game/gui/MapPage.java
@@ -2,12 +2,18 @@ package name.martingeisse.trading_game.gui;
 
 import name.martingeisse.trading_game.gui.wicket.page.AbstractPage;
 import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.attributes.CallbackParameter;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.PriorityHeaderItem;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.resource.JQueryResourceReference;
 
 import java.util.ArrayList;
@@ -21,8 +27,20 @@ public class MapPage extends AbstractPage {
 
 	private final List<MapObject> mapObjects = new ArrayList<>();
 
+	private String testText;
+
 	public MapPage() {
-		add(new ListView<MapObject>("mapObjects", mapObjects) {
+
+		// test data TODO remove
+		Random random = new Random();
+		for (int i=0; i<100; i++) {
+			mapObjects.add(new MapObject(random.nextLong() % 1000, random.nextLong() % 1000));
+		}
+
+		// map
+		WebMarkupContainer map = new WebMarkupContainer("map");
+		add(map);
+		map.add(new ListView<MapObject>("mapObjects", mapObjects) {
 			@Override
 			protected void populateItem(ListItem<MapObject> item) {
 				MapObject mapObject = item.getModelObject();
@@ -30,10 +48,38 @@ public class MapPage extends AbstractPage {
 				item.add(new Label("text", "" + mapObject.getX() + "," + mapObject.getY()));
 			}
 		});
-		Random random = new Random();
-		for (int i=0; i<100; i++) {
-			mapObjects.add(new MapObject(random.nextLong() % 1000, random.nextLong() % 1000));
-		}
+		map.add(new AbstractDefaultAjaxBehavior() {
+
+			@Override
+			public void renderHead(Component component, IHeaderResponse response) {
+				super.renderHead(component, response);
+				String script = "onSelectMapObject = " + getCallbackFunction(CallbackParameter.explicit("testText"));
+				response.render(JavaScriptHeaderItem.forScript(script, null));
+			}
+
+			@Override
+			protected void respond(AjaxRequestTarget target) {
+				testText = getRequest().getRequestParameters().getParameterValue("testText").toString();
+				target.add(MapPage.this.get("rightSidebar"));
+			}
+
+		});
+
+		// right sidebar
+		WebMarkupContainer rightSidebar = new WebMarkupContainer("rightSidebar");
+		rightSidebar.setOutputMarkupId(true);
+		add(rightSidebar);
+		rightSidebar.add(new Label("test", new PropertyModel<>(this, "testText")));
+
+	}
+
+	/**
+	 * Getter method.
+	 *
+	 * @return the testText
+	 */
+	public String getTestText() {
+		return testText;
 	}
 
 	@Override
