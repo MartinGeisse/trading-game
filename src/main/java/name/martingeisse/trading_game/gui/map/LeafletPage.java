08fd99d30f7449ca4f9a24f8a44ab7e111208ef2
Martin Geisse <martin.geisse@servicereisen.de>
2017-01-24 08:08:34 +0100

    map click support, WIP

--- src/main/java/name/martingeisse/trading_game/gui/map/LeafletPage.java
+++ src/main/java/name/martingeisse/trading_game/gui/map/LeafletPage.java
@@ -2,6 +2,10 @@ package name.martingeisse.trading_game.gui.map;
 
 import name.martingeisse.trading_game.gui.leaflet.Leaflet;
 import name.martingeisse.trading_game.gui.wicket.page.AbstractPage;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.attributes.CallbackParameter;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.request.Url;
@@ -16,6 +20,47 @@ public class LeafletPage extends AbstractPage {
 
 	public LeafletPage(PageParameters parameters) {
 		super(parameters);
+		add(new AbstractDefaultAjaxBehavior() {
+
+			@Override
+			public void renderHead(Component component, IHeaderResponse response) {
+				super.renderHead(component, response);
+
+				// build callback functions
+				{
+					StringBuilder builder = new StringBuilder();
+
+					// clicking on the map
+					{
+						CallbackParameter[] parameters = {
+							CallbackParameter.resolved("command", "'click'"),
+							CallbackParameter.explicit("x"),
+							CallbackParameter.explicit("y"),
+						};
+						builder.append("sendMapClickCommand = ").append(getCallbackFunction(parameters)).append(';');
+					}
+
+					response.render(JavaScriptHeaderItem.forScript(builder, null));
+				}
+			}
+
+			@Override
+			protected void respond(AjaxRequestTarget target) {
+				String command = target.getPageParameters().get("command").toString();
+				if (command == null) {
+					return;
+				}
+				switch (command) {
+					case "click": {
+						int x = target.getPageParameters().get("x").toInt();
+						int y = target.getPageParameters().get("y").toInt();
+						System.out.println("clicked at " + x + ", " + y);
+						break;
+					}
+				}
+			}
+
+		});
 	}
 
 	@Override
