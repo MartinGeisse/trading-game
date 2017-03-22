242eb0c43a3a681da1bfb811eb332f62d0bfbd44
Martin Geisse <martingeisse@googlemail.com>
2017-03-22 19:04:29 +0100

    login token support, WIP

--- src/main/java/name/martingeisse/trading_game/platform/wicket/MyWicketSession.java
+++ src/main/java/name/martingeisse/trading_game/platform/wicket/MyWicketSession.java
@@ -4,13 +4,18 @@ import name.martingeisse.trading_game.game.player.Player;
 import name.martingeisse.trading_game.game.player.PlayerRepository;
 import org.apache.wicket.protocol.http.WebSession;
 import org.apache.wicket.request.Request;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.http.WebRequest;
+import org.apache.wicket.request.http.WebResponse;
+
+import javax.servlet.http.Cookie;
 
 /**
  *
  */
 public class MyWicketSession extends WebSession {
 
-	private long playerId = -1;
+	private Long playerId = null;
 
 	/**
 	 * Constructor.
@@ -28,22 +33,60 @@ public class MyWicketSession extends WebSession {
 		return (MyWicketSession) WebSession.get();
 	}
 
-	/**
-	 * Getter method.
-	 *
-	 * @return the playerId
-	 */
-	public long getPlayerId() {
+	public void createPlayer() {
+		playerId = MyWicketApplication.get().getDependency(PlayerRepository.class).createPlayer();
+	}
+
+	public void determinePlayerIdFromCookie() {
+		playerId = null;
+		WebRequest request = (WebRequest) RequestCycle.get().getRequest();
+		Cookie providedLoginStringCookie = request.getCookie("login");
+		if (providedLoginStringCookie != null) {
+			String providedLoginString = providedLoginStringCookie.getValue();
+			if (providedLoginString != null) {
+				int index = providedLoginString.indexOf(':');
+				if (index != -1) {
+					long providedPlayerId;
+					try {
+						providedPlayerId = Long.parseLong(providedLoginString.substring(0, index));
+					} catch (NumberFormatException e) {
+						return;
+					}
+					String providedLoginToken = providedLoginString.substring(index + 1);
+					Player player = MyWicketApplication.get().getDependency(PlayerRepository.class).getPlayerById(providedPlayerId);
+					if (player != null && player.getLoginToken().equals(providedLoginToken)) {
+						playerId = providedPlayerId;
+					}
+				}
+			}
+		}
+	}
+
+	public void storePlayerIdToCookie() {
+		Player player = getPlayer();
+		String loginString = (player == null ? "" : (player.getId() + ":" + player.getLoginToken()));
+		WebResponse response = (WebResponse) RequestCycle.get().getResponse();
+		Cookie cookie = new Cookie("login", loginString);
+		cookie.setMaxAge(10 * 365 * 24 * 60 * 60);
+		cookie.setPath("/");
+		response.addCookie(cookie);
+	}
+
+	public Long getPlayerId() {
 		return playerId;
 	}
 
 	public Player getPlayer() {
 		bind();
-		PlayerRepository playerRepository = MyWicketApplication.get().getDependency(PlayerRepository.class);
-		if (playerId < 0) {
-			playerId = playerRepository.createPlayer();
+		if (playerId == null) {
+			return null;
+		} else {
+			return MyWicketApplication.get().getDependency(PlayerRepository.class).getPlayerById(playerId);
 		}
-		return playerRepository.getPlayerById(playerId);
+	}
+
+	public void unsetPlayerId() {
+		playerId = null;
 	}
 
 }
