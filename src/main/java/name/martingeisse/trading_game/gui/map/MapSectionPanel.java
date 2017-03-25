f4beabf4cb0566ee50f09827c9fec4b52a03da03
Martin Geisse <martingeisse@googlemail.com>
2017-03-25 20:04:35 +0100

    changed map side bar to a small properties box

--- src/main/java/name/martingeisse/trading_game/gui/map/MapSectionPanel.java
+++ src/main/java/name/martingeisse/trading_game/gui/map/MapSectionPanel.java
@@ -27,6 +27,7 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.attributes.CallbackParameter;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.core.request.handler.IPartialPageRequestHandler;
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
@@ -46,7 +47,9 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Encapsulates the actual map view and the right sidebar.
+ * Encapsulates the actual map view and the properties box.
+ *
+ * TODO remove unneeded methods and game event listeners
  */
 public class MapSectionPanel extends AbstractPanel implements GuiGameEventListener {
 
@@ -61,17 +64,30 @@ public class MapSectionPanel extends AbstractPanel implements GuiGameEventListen
 	public MapSectionPanel(String id) {
 		super(id);
 
-		IModel<SpaceObject> selectedSpaceObjectModel = new PropertyModel<>(this, "selectedSpaceObject");
-
-		WebMarkupContainer sidebar = new WebMarkupContainer("sidebar");
-		sidebar.setOutputMarkupId(true);
-		add(sidebar);
-		sidebar.add(new Label("name", new PropertyModel<>(this, "selectedSpaceObject.name")));
-		sidebar.add(new Label("type", new PropertyModel<>(this, "selectedSpaceObject.class.simpleName")));
-		sidebar.add(new Label("x", new PropertyModel<>(this, "selectedSpaceObject.x")));
-		sidebar.add(new Label("y", new PropertyModel<>(this, "selectedSpaceObject.y")));
-		sidebar.add(new Label("distance", new PropertyModel<>(this, "selectedSpaceObjectDistance")));
-		sidebar.add(new ListView<Action>("actions", new PropertyModel<>(this, "selectedSpaceObjectActions")) {
+		WebMarkupContainer propertiesBox = new WebMarkupContainer("propertiesBox") {
+			@Override
+			protected void onComponentTag(ComponentTag tag) {
+				super.onComponentTag(tag);
+				String classAttribute = tag.getAttribute("class");
+				if (classAttribute != null) {
+					classAttribute = classAttribute.replace("hasSelectedSpaceObject", "").trim();
+				} else {
+					classAttribute = "";
+				}
+				if (getSelectedSpaceObject() != null) {
+					classAttribute = classAttribute + " hasSelectedSpaceObject";
+				}
+				tag.put("class", classAttribute);
+			}
+		};
+		propertiesBox.setOutputMarkupId(true);
+		add(propertiesBox);
+		propertiesBox.add(new Label("name", new PropertyModel<>(this, "selectedSpaceObject.name")));
+		propertiesBox.add(new Label("type", new PropertyModel<>(this, "selectedSpaceObject.class.simpleName")));
+		propertiesBox.add(new Label("x", new PropertyModel<>(this, "selectedSpaceObject.x")));
+		propertiesBox.add(new Label("y", new PropertyModel<>(this, "selectedSpaceObject.y")));
+		propertiesBox.add(new Label("distance", new PropertyModel<>(this, "selectedSpaceObjectDistance")));
+		propertiesBox.add(new ListView<Action>("actions", new PropertyModel<>(this, "selectedSpaceObjectActions")) {
 			@Override
 			protected void populateItem(ListItem<Action> actionItem) {
 				AjaxLink<?> link = new AjaxLink<Void>("link") {
@@ -87,112 +103,6 @@ public class MapSectionPanel extends AbstractPanel implements GuiGameEventListen
 				actionItem.add(link);
 			}
 
-		});
-		WebMarkupContainer remoteItemsContainer = new WebMarkupContainer("remoteItemsContainer") {
-			@Override
-			protected void onConfigure() {
-				super.onConfigure();
-				setVisible(getRemoteItems() != null);
-			}
-		};
-		sidebar.add(remoteItemsContainer);
-		remoteItemsContainer.add(new ListView<ImmutableItemStack>("itemStacks", new PropertyModel<>(this, "remoteItems")) {
-			@Override
-			protected void populateItem(ListItem<ImmutableItemStack> item) {
-				item.add(new Label("size", "" + item.getModelObject().getSize()));
-				item.add(new Label("itemType", "" + item.getModelObject().getItemType()));
-				item.add(new Image("icon", ItemIcons.get(item.getModelObject().getItemType())));
-				item.add(new AjaxLink<Void>("loadLink") {
-					@Override
-					public void onClick(AjaxRequestTarget target) {
-						Player player = getPlayer();
-						SpaceStation spaceStation = (SpaceStation) getSelectedSpaceObject();
-						ImmutableItemStack itemsToLoad = new ImmutableItemStack(item.getModelObject().getItemType(), item.getModelObject().getSize());
-						ActionQueue actionQueue = player.getActionQueue();
-						actionQueue.cancelCurrentAction();
-						actionQueue.cancelAllPendingActions();
-						actionQueue.scheduleAction(new LoadUnloadAction(player, spaceStation, LoadUnloadAction.Type.LOAD, itemsToLoad, item.getIndex()));
-					}
-				});
-			}
-		});
-		WebMarkupContainer localItemsContainer = new WebMarkupContainer("localItemsContainer") {
-			@Override
-			protected void onConfigure() {
-				super.onConfigure();
-				setVisible(getLocalItems() != null);
-			}
-		};
-		sidebar.add(localItemsContainer);
-		localItemsContainer.add(new ListView<ImmutableItemStack>("itemStacks", new PropertyModel<>(this, "localItems")) {
-			@Override
-			protected void populateItem(ListItem<ImmutableItemStack> item) {
-				item.add(new Label("size", "" + item.getModelObject().getSize()));
-				item.add(new Label("itemType", "" + item.getModelObject().getItemType()));
-				item.add(new Image("icon", ItemIcons.get(item.getModelObject().getItemType())));
-				item.add(new AjaxLink<Void>("unloadLink") {
-					@Override
-					public void onClick(AjaxRequestTarget target) {
-						Player player = getPlayer();
-						SpaceStation spaceStation = (SpaceStation) getSelectedSpaceObject();
-						ImmutableItemStack itemsToLoad = new ImmutableItemStack(item.getModelObject().getItemType(), item.getModelObject().getSize());
-						ActionQueue actionQueue = player.getActionQueue();
-						actionQueue.cancelCurrentAction();
-						actionQueue.cancelAllPendingActions();
-						actionQueue.scheduleAction(new LoadUnloadAction(player, spaceStation, LoadUnloadAction.Type.UNLOAD, itemsToLoad, item.getIndex()));
-					}
-				});
-				item.add(new AjaxLink<Void>("equipLink") {
-
-					@Override
-					protected void onConfigure() {
-						super.onConfigure();
-						setVisible(item.getModelObject().getItemType().getPlayerShipEquipmentSlotType() != null);
-					}
-
-					@Override
-					public void onClick(AjaxRequestTarget target) {
-						Player player = getPlayer();
-						ActionQueue actionQueue = player.getActionQueue();
-						// TODO should add this action as the next one but keep other actions
-						actionQueue.cancelCurrentAction();
-						actionQueue.cancelAllPendingActions();
-						actionQueue.scheduleAction(new EquipAction(player, item.getModelObject().getItemType()));
-					}
-
-				});
-			}
-		});
-
-
-		WebMarkupContainer equipmentContainer = new WebMarkupContainer("equipmentContainer") {
-			@Override
-			protected void onConfigure() {
-				super.onConfigure();
-				setVisible(getLocalItems() != null);
-			}
-		};
-		sidebar.add(equipmentContainer);
-		equipmentContainer.add(new ListView<SlotInfo>("slots", new PropertyModel<>(this, "equipmentSlots")) {
-			@Override
-			protected void populateItem(ListItem<SlotInfo> item) {
-				item.add(new Label("slotType", item.getModelObject().getPlayerShipEquipmentSlotType().name().toLowerCase()));
-				item.add(new Label("itemType", "" + item.getModelObject().getItemType()));
-				item.add(new Image("icon", ItemIcons.get(item.getModelObject().getItemType())));
-				item.add(new AjaxLink<Void>("unequipLink") {
-
-					@Override
-					public void onClick(AjaxRequestTarget target) {
-						Player player = getPlayer();
-						ActionQueue actionQueue = player.getActionQueue();
-						// TODO should add this action as the next one but keep other actions
-						actionQueue.cancelCurrentAction();
-						actionQueue.cancelAllPendingActions();
-						actionQueue.scheduleAction(new UnequipAction(player, item.getModelObject().getPlayerShipEquipmentSlotType()));
-					}
-
-				});
-			}
 		});
 
 		add(new AbstractDefaultAjaxBehavior() {
@@ -280,9 +190,10 @@ public class MapSectionPanel extends AbstractPanel implements GuiGameEventListen
 							target.appendJavaScript("setStateCookie('mapSelection', " + selectedSpaceObjectId + ")"); // TODO prevent conversion from long to float in JS
 						} else {
 							selectedSpaceObjectId = -1;
+							target.appendJavaScript("removeSpaceObjectSelectionIndicator();");
 							target.appendJavaScript("setStateCookie('mapSelection', null)");
 						}
-						target.add(sidebar);
+						target.add(propertiesBox);
 						if (command.equals("menu")) {
 							int x = parameters.getParameterValue("x").toInt();
 							int y = parameters.getParameterValue("y").toInt();
@@ -301,7 +212,7 @@ public class MapSectionPanel extends AbstractPanel implements GuiGameEventListen
 						double indicatorRadius = MapCoordinates.convertGameDistanceToMapDistance(5000) + 4 * Math.pow(0.5, zoom);
 						target.appendJavaScript("changeSpaceObjectSelectionIndicator(" + indicatorLatitude + ", " + indicatorLongitude + ", " + indicatorRadius + ");");
 						target.appendJavaScript("setStateCookie('mapSelection', " + selectedSpaceObjectId + ")"); // TODO prevent conversion from long to float in JS
-						target.add(sidebar);
+						target.add(propertiesBox);
 						break;
 					}
 
@@ -320,7 +231,11 @@ public class MapSectionPanel extends AbstractPanel implements GuiGameEventListen
 							double indicatorRadius = MapCoordinates.convertGameDistanceToMapDistance(5000) + 4 * Math.pow(0.5, zoom);
 							target.appendJavaScript("changeSpaceObjectSelectionIndicator(" + indicatorLatitude + ", " + indicatorLongitude + ", " + indicatorRadius + ");");
 							target.appendJavaScript("setStateCookie('mapSelection', " + selectedSpaceObjectId + ")"); // TODO prevent conversion from long to float in JS
-							target.add(sidebar);
+							target.add(propertiesBox);
+						} else {
+							selectedSpaceObjectId = -1;
+							target.appendJavaScript("removeSpaceObjectSelectionIndicator();");
+							target.appendJavaScript("setStateCookie('mapSelection', null)");
 						}
 						break;
 					}
@@ -431,12 +346,12 @@ public class MapSectionPanel extends AbstractPanel implements GuiGameEventListen
 
 		// analyze events
 		boolean anySpaceObjectChangedItsPosition = false;
-		boolean sidebarChanged = false;
+		boolean propertiesBoxChanged = false;
 		for (GameEvent event : events) {
 			if (event instanceof SpaceObjectPositionChangedEvent) {
 				anySpaceObjectChangedItsPosition = true;
 				if (((SpaceObjectPositionChangedEvent) event).getId() == selectedSpaceObjectId) {
-					sidebarChanged = true;
+					propertiesBoxChanged = true;
 				}
 			} else if (event instanceof InventoryChangedEvent) {
 				SpaceObject selectedSpaceObject = getSelectedSpaceObject();
@@ -444,13 +359,13 @@ public class MapSectionPanel extends AbstractPanel implements GuiGameEventListen
 					long eventInventoryId = ((InventoryChangedEvent) event).getInventoryId();
 					long selectedSpaceObjectInventoryId = ((ObjectWithInventory) selectedSpaceObject).getInventoryId();
 					if (eventInventoryId == selectedSpaceObjectInventoryId || eventInventoryId == getPlayer().getInventory().getId()) {
-						sidebarChanged = true;
+						propertiesBoxChanged = true;
 					}
 				}
 			} else if (event instanceof PlayerShipEquipmentChangedEvent) {
 				long playerShipId = ((PlayerShipEquipmentChangedEvent) event).getPlayerShipId();
 				if (playerShipId == getPlayer().getShip().getId()) {
-					sidebarChanged = true;
+					propertiesBoxChanged = true;
 				}
 			}
 		}
@@ -464,8 +379,8 @@ public class MapSectionPanel extends AbstractPanel implements GuiGameEventListen
 		}
 
 		// react to changes in the side bar
-		if (sidebarChanged) {
-			partialPageRequestHandler.add(get("sidebar"));
+		if (propertiesBoxChanged) {
+			partialPageRequestHandler.add(get("propertiesBox"));
 		}
 	}
 
