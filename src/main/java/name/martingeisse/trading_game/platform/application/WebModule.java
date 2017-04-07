da46ab125ca24f61ff55d6c6d23d0ef58cdf056a
Martin Geisse <martingeisse@googlemail.com>
2017-04-07 17:26:04 +0200

    assign readable name to running web threads

--- src/main/java/name/martingeisse/trading_game/platform/application/WebModule.java
+++ src/main/java/name/martingeisse/trading_game/platform/application/WebModule.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2015 Martin Geisse
- *
+ * <p>
  * This file is distributed under the terms of the MIT license.
  */
 
@@ -12,10 +12,12 @@ import com.google.inject.servlet.ServletModule;
 import name.martingeisse.trading_game.platform.postgres.PostgresContextService;
 import name.martingeisse.trading_game.platform.wicket.MyWicketApplication;
 import name.martingeisse.trading_game.platform.wicket.MyWicketFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.protocol.http.WicketFilter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,12 +27,14 @@ import java.util.Map;
  */
 public class WebModule extends ServletModule {
 
+	public static final String THREAD_NAME_PREFIX = "web";
+
 	// override
 	@Override
 	protected void configureServlets() {
 
 		// reset the per-thread PostgresContext after each request, so the next request uses a fresh state
-		filterRegex("/.*").through(PostgresContextFilter.class);
+		filterRegex("/.*").through(MyFilter.class);
 
 		// bind Wicket
 		{
@@ -40,16 +44,16 @@ public class WebModule extends ServletModule {
 			initParams.put(WicketFilter.FILTER_MAPPING_PARAM, "/*");
 			filterRegex("/.*").through(MyWicketFilter.class, initParams);
 		}
-		
+
 	}
 
 	@Singleton
-	public static class PostgresContextFilter implements Filter {
+	public static class MyFilter implements Filter {
 
 		private final PostgresContextService postgresContextService;
 
 		@Inject
-		public PostgresContextFilter(PostgresContextService postgresContextService) {
+		public MyFilter(PostgresContextService postgresContextService) {
 			this.postgresContextService = postgresContextService;
 		}
 
@@ -64,12 +68,30 @@ public class WebModule extends ServletModule {
 		@Override
 		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 			try {
+				Thread.currentThread().setName(THREAD_NAME_PREFIX + '-' + getThreadNameSuffix(request));
 				chain.doFilter(request, response);
 			} finally {
 				postgresContextService.reset();
+				Thread.currentThread().setName(THREAD_NAME_PREFIX);
 			}
 		}
 
+	}
+
+	private static String getThreadNameSuffix(ServletRequest untypedRequest) {
+		if (untypedRequest instanceof HttpServletRequest) {
+			HttpServletRequest request = (HttpServletRequest) untypedRequest;
+			String requestUri = request.getRequestURI();
+			StringBuilder builder = new StringBuilder();
+			for (int i=0; i<requestUri.length(); i++) {
+				char c = requestUri.charAt(i);
+				if (c == '/' || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+					builder.append(c);
+				}
+			}
+			return builder.toString();
+		}
+		return "unknown";
 	}
 
 }
