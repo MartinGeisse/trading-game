c0784cbbef95468f3b20051a392f1af919855025
Martin Geisse <martingeisse@googlemail.com>
2017-05-25 11:47:48 +0200

    save feedback to database

--- src/main/java/name/martingeisse/trading_game/peripherals/feedback/SubmitFeedbackService.java
+++ src/main/java/name/martingeisse/trading_game/peripherals/feedback/SubmitFeedbackService.java
@@ -1,6 +1,11 @@
 package name.martingeisse.trading_game.peripherals.feedback;
 
+import com.google.inject.Inject;
+import com.querydsl.sql.dml.SQLInsertClause;
 import name.martingeisse.trading_game.game.jackson.JacksonService;
+import name.martingeisse.trading_game.platform.postgres.PostgresContextService;
+import name.martingeisse.trading_game.postgres_entities.QGameFeedbackRow;
+import name.martingeisse.trading_game.tools.codegen.PostgresJsonb;
 
 /**
  *
@@ -8,14 +13,24 @@ import name.martingeisse.trading_game.game.jackson.JacksonService;
 public class SubmitFeedbackService {
 
 	private final JacksonService jacksonService;
+	private final PostgresContextService postgresContextService;
 
-	public SubmitFeedbackService(JacksonService jacksonService) {
+	@Inject
+	public SubmitFeedbackService(JacksonService jacksonService, PostgresContextService postgresContextService) {
 		this.jacksonService = jacksonService;
+		this.postgresContextService = postgresContextService;
 	}
 
-	public void submitFeedback(String sessionId, long playerId, FeedbackContext context, String text) {
+	public void submitFeedback(String sessionId, Long playerId, FeedbackContext context, String text) {
+		QGameFeedbackRow qf = QGameFeedbackRow.GameFeedback;
+
 		String serializedContext = jacksonService.serialize(context);
-		System.out.println("feedback by session " + sessionId + ", playerId " + playerId + ", context " + serializedContext + ": " + text);
+		SQLInsertClause insert = postgresContextService.insert(qf);
+		insert.set(qf.sessionId, sessionId);
+		insert.set(qf.playerId, playerId);
+		insert.set(qf.context, new PostgresJsonb(serializedContext));
+		insert.set(qf.text, text);
+		insert.execute();
 	}
 
 }
